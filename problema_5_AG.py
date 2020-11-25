2c56120c94373b53c2188ddc7f8d213cc1101844
GutuAlexei <70639790+GutuAlexei@users.noreply.github.com>
2020-11-25 18:31:09 +0200

    Delete problema_5_AG.py

--- problema_5_AG.py
+++
@@ -1,8 +0,0 @@
-n=str(input('CNP persoanei este '))
-if len(n)==13:
-    i=0
-    while  ord(n) in range (48 , 58):
-        i+=1
-    print('CNP este corect')
-else:
-    print('CNp nu este corect')
